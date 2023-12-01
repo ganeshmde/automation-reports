673e02fae96a40bfffd6a5fa31a13528193aafe2
Ganesh <90398683+ganeshmde@users.noreply.github.com>
2023-12-01 21:19:40 +0530

    Update ExtentReportsOld.cs

--- Reports/ExtentReportsOld.cs
+++ Reports/ExtentReportsOld.cs
@@ -1,4 +1,5 @@
 using System.Configuration;
+using System.Globalization;
 using System.Xml;
 using AventStack.ExtentReports;
 using AventStack.ExtentReports.Gherkin.Model;
@@ -10,57 +11,93 @@ namespace Reports.Old
 {
     class ExtentReportsOld
     {
-        public static ExtentReports extent;
-        public static ExtentHtmlReporter htmlReporter;
-        public static List<TestFeature> features = new List<TestFeature>();
-        public static string[] testSuiteFiles;
-        static string allureResultsDir = ConfigurationManager.AppSettings.Get("allure-results");
+        ExtentReports extent;
+        ExtentHtmlReporter htmlReporter;
+        List<TestFeature> features = new List<TestFeature>();
+        string[] testSuiteFiles;
+        string allureResultsDir = ConfigurationManager.AppSettings.Get("allure-results");
 
-        public static void ImplementReports()
+        public ExtentReportsOld()
         {
-            if (extent == null)
-            {
-                string reportsPath = GetReportsPath();
-                htmlReporter = new ExtentHtmlReporter(reportsPath);
-                extent = new ExtentReports();
-                extent.AttachReporter(htmlReporter);
-                //Reports Configuration
-                htmlReporter.Config.DocumentTitle = "Strategic Space Automation Report";
-                htmlReporter.Config.ReportName = "Automation Test Report";
-                htmlReporter.Config.Theme = Theme.Standard;
-                htmlReporter.Config.Encoding = "UTF-8";
-            }
+            GenearateReports();
         }
-        static string GetReportsPath()
+
+        void GenearateReports()
+        {
+            ImplementReports();
+            GetTestSuiteFiles();
+            GetXmlData();
+            CreateTestNodes();
+            extent.Flush();
+        }
+
+        /// <summary>
+        /// Attach htmlreporter and set the configuration
+        /// </summary>
+        void ImplementReports()
+        {
+            string reportsPath = GetReportsPath();
+            htmlReporter = new ExtentHtmlReporter(reportsPath);
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+            //Reports Configuration
+            htmlReporter.Config.DocumentTitle = "Strategic Space Automation Report";
+            htmlReporter.Config.ReportName = "Automation Reports";
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Config.Encoding = "UTF-8";
+        }
+
+        /// <summary>
+        /// Creates html report file with current date as name
+        /// </summary>
+        /// <returns></returns>
+        string GetReportsPath()
         {
             string codeBasePath = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
             string directory = Path.GetDirectoryName(codeBasePath);
             string projectPath = new Uri(directory).LocalPath;
             string reportsDir = projectPath + "\\reports";
 
-            if (Directory.Exists(reportsDir))
+            if (!Directory.Exists(reportsDir))
             {
-                Directory.Delete(reportsDir, true);
+                Directory.CreateDirectory(reportsDir);
             }
 
-            Directory.CreateDirectory(reportsDir);
+            DateTime dt = DateTime.Now;
+            string date = dt.Day.ToString() + "_" + GetMonthName(dt) + "_" + dt.Year.ToString();
+            string reportName = $"index_{date}.html";
 
-            return reportsDir + "\\index.html";
+            return reportsDir + reportName;
         }
 
-        public static void GetTestSuiteFiles()
+        /// <summary>
+        /// Returns Abbreviated Month name for the given date
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        string GetMonthName(DateTime dateTime)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(dateTime.Month);
+        }
+
+        /// <summary>
+        /// Stores the paths of test suite xml files
+        /// </summary>
+        void GetTestSuiteFiles()
         {
             string[] files = Directory.GetFiles(allureResultsDir);
             testSuiteFiles = files.Where(x => x.Contains("-testsuite.xml")).ToArray();
         }
 
-        public static void GetXmlData()
+        /// <summary>
+        /// Reads the xml and gets the features, scenarios and steps related info
+        /// </summary>
+        void GetXmlData()
         {
             foreach (string file in testSuiteFiles)
             {
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.Load(file);
-
                 TestFeature feature = new TestFeature();
                 feature.name = xmlDoc.SelectSingleNode("//name").InnerText;
                 feature.tests = GetTestCases(xmlDoc);
@@ -68,7 +105,12 @@ namespace Reports.Old
             }
         }
 
-        static List<Testcase> GetTestCases(XmlNode node)
+        /// <summary>
+        /// Gets the Scenario/testcase info
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        List<Testcase> GetTestCases(XmlNode node)
         {
             XmlNodeList testcaseNodes = node.SelectNodes("//test-case");
             List<Testcase> testcases = new List<Testcase>();
@@ -76,45 +118,51 @@ namespace Reports.Old
             foreach (XmlNode tc in testcaseNodes)
             {
                 Testcase test = new Testcase();
-                long startTime = long.Parse(tc.Attributes["start"].InnerText);
-                long stopTime = long.Parse(tc.Attributes["stop"].InnerText);
+                double startTime = double.Parse(tc.Attributes["start"].InnerText);
+                double stopTime = double.Parse(tc.Attributes["stop"].InnerText);
                 string status = tc.Attributes["status"].InnerText;
                 string scenario = tc.FirstChild.InnerText;
+                string error = null;
 
                 if (status == "broken" || status == "failed")
                 {
-                    string errorInfo = tc.LastChild.LastChild.FirstChild.InnerText;
-                    test.Error = errorInfo;
+                    error = tc.LastChild.LastChild.FirstChild.InnerText;
                 }
-
-
                 test.Status = status;
                 test.StartTime = startTime;
                 test.EndTime = stopTime;
                 test.Scenario = scenario;
                 test.Steps = GetSteps(tc);
-
+                test.Error = error;
                 testcases.Add(test);
             }
 
             return testcases;
         }
 
-        static List<Step> GetSteps(XmlNode node)
+        /// <summary>
+        /// Gets the steps info corresponding to the scenario
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        List<Step> GetSteps(XmlNode node)
         {
-            XmlNodeList stepNodes = node.Cast<XmlNode>().Where(x => x.Name == "steps").Select(x => x.ChildNodes).FirstOrDefault();
+            XmlNodeList stepNodes = node.Cast<XmlNode>()
+                .Where(x => x.Name == "steps")
+                .Select(x => x.ChildNodes)
+                .FirstOrDefault();
             List<Step> steps = new List<Step>();
 
-            foreach (XmlNode stepNode in stepNodes)
+            foreach (XmlNode st in stepNodes)
             {
                 Step step = new Step();
-                long startTime = long.Parse(stepNode.Attributes["start"].InnerText);
-                long stopTime = long.Parse(stepNode.Attributes["stop"].InnerText);
-                string status = stepNode.Attributes["status"].InnerText;
-                string stepInfo = stepNode.FirstChild.InnerText;
+                long startTime = long.Parse(st.Attributes["start"].InnerText);
+                long stopTime = long.Parse(st.Attributes["stop"].InnerText);
+                string status = st.Attributes["status"].InnerText;
+                string stepInfo = st.FirstChild.InnerText;
                 string[] arr = stepInfo.Split(" ");
                 string info = string.Join(" ", arr.Skip(1));
-                XmlNode attatchments = stepNode.ChildNodes.Item(2);
+                XmlNode attatchments = st.ChildNodes.Item(2);
                 XmlNode attachment = attatchments.FirstChild;
                 string imageName = null;
 
@@ -122,7 +170,6 @@ namespace Reports.Old
                 {
                     imageName = attachment.Attributes["source"].InnerText;
                 }
-
 
                 step.StartTime = startTime;
                 step.EndTime = stopTime;
@@ -136,7 +183,10 @@ namespace Reports.Old
             return steps;
         }
 
-        public static void GenerateReport()
+        /// <summary>
+        /// Creates test nodes using the tests info that read from xml files
+        /// </summary>
+        void CreateTestNodes()
         {
             foreach (TestFeature feature in features)
             {
@@ -145,7 +195,7 @@ namespace Reports.Old
             }
         }
 
-        static void CreateScenario(ExtentTest extentTest, TestFeature feature)
+        void CreateScenario(ExtentTest extentTest, TestFeature feature)
         {
             List<Testcase> testcases = feature.tests;
             foreach (var test in testcases)
@@ -156,7 +206,7 @@ namespace Reports.Old
 
         }
 
-        static void CreateStep(ExtentTest extentTest, Testcase test)
+        void CreateStep(ExtentTest extentTest, Testcase test)
         {
             List<Step> steps = test.Steps;
 
@@ -225,14 +275,15 @@ namespace Reports.Old
 
             }
         }
-        static MediaEntityModelProvider GetScreenshot(string imageName)
+
+        /// <summary>
+        /// Returns failed scenario screenshot by its image name
+        /// </summary>
+        /// <param name="imageName"></param>
+        /// <returns></returns>
+        MediaEntityModelProvider GetScreenshot(string imageName)
         {
             return MediaEntityBuilder.CreateScreenCaptureFromPath(allureResultsDir + "\\" + imageName).Build();
-        }
-
-        public static void FlushReport()
-        {
-            extent.Flush();
         }
     }
 }
