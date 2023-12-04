d7afb190e0898fc7519a5a48bc631d13bf420d23
Ganesh <90398683+ganeshmde@users.noreply.github.com>
2023-12-04 06:04:43 +0530

    Update NewExtent.cs

--- Extent/NewExtent.cs
+++ Extent/NewExtent.cs
@@ -16,11 +16,11 @@ namespace Reports.Extent
         {
             if (xmlFiles.Length > 0)
             {
-                new ExtractXmlTestData(xmlFiles, out features);
+                new ExtractTestDataFromXml(xmlFiles, out features);
             }
             else if (jsonFiles.Length > 0)
             {
-                new ExtractJsonTestData(jsonFiles, allureResultsDir, out features);
+                new ExtractTestDataFromJson(jsonFiles, allureResultsDir, out features);
             }
             else
             {
